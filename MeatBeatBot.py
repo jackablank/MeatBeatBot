fdf812413213ffcaeec4b6a427975fdeb4af7739
jackablank <69688037+jackablank@users.noreply.github.com>
2024-12-31 00:16:59 -0800

    Add files via upload

---
+++ MeatBeatBot.py
@@ -0,0 +1,9 @@
+
+def main():
+    print("Hello World!")
+    print("This is the Meat Beat Bot!")
+    print("I am here to help you with all your meat beating needs!")
+
+
+if __name__ == "__main__":
+    main()
