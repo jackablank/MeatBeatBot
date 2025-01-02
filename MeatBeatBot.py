243c3867e4b06f1e4e2867cac52db8a95d5e5b0b
jackablank <jackablank@gmail.com>
2025-01-02 05:13:52 -0800

    On branch test-branch-1  Changes to be committed: 	modified:   .gitignore modified:   MeatBeatBot.py 	modified:   botkeys.env

--- MeatBeatBot.py
+++ MeatBeatBot.py
@@ -6,17 +6,16 @@ import os
 def main():
     load_dotenv(dotenv_path='C:/MeatBeatBot/botkeys.env')
     botToken = os.getenv('BOT_TOKEN')
+    testChannelID = os.getenv('TEST_CHANNEL_ID')
 
-    BOT_TOKEN = "MTMyNDMwNjA1MTg1MTQ4NTIwNQ.GNjVpW.JYqWvedY0pPkvtqOOLjaX90qyARNFxmzH7TLXU"
-    JERK_OFF_CHANNEL_ID = 1324309720026320978
-    TEST_CHANNEL_ID = 1324309933239308300
+    
 
     bot = commands.Bot(command_prefix='//', intents=discord.Intents.all())
     
     @bot.event
     async def on_ready():
         print(f'{bot.user} has connected to Discord!')
-        channel = bot.get_channel(TEST_CHANNEL_ID)
+        channel = bot.get_channel(int(testChannelID))
         await channel.send("jerk off time")
 
     @bot.command()
