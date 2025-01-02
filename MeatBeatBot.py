8113f343bd90ad9cbf83de53c7fa7ec08ed69142
jackablank <jackablank@gmail.com>
2025-01-02 04:37:18 -0800

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   MeatBeatBot.py
    	new file:   botkeys.env

--- MeatBeatBot.py
+++ MeatBeatBot.py
@@ -1,6 +1,31 @@
+from discord.ext import commands
+import discord
+from dotenv import load_dotenv
+import os
 
 def main():
-    print("Please pretty please commit properly")
+    load_dotenv(dotenv_path='C:/MeatBeatBot/botkeys.env')
+    botToken = os.getenv('BOT_TOKEN')
+
+    BOT_TOKEN = "MTMyNDMwNjA1MTg1MTQ4NTIwNQ.GNjVpW.JYqWvedY0pPkvtqOOLjaX90qyARNFxmzH7TLXU"
+    JERK_OFF_CHANNEL_ID = 1324309720026320978
+    TEST_CHANNEL_ID = 1324309933239308300
+
+    bot = commands.Bot(command_prefix='//', intents=discord.Intents.all())
+    
+    @bot.event
+    async def on_ready():
+        print(f'{bot.user} has connected to Discord!')
+        channel = bot.get_channel(TEST_CHANNEL_ID)
+        await channel.send("jerk off time")
+
+    @bot.command()
+    async def add_phrase(ctx):
+        await ctx.send("Phrase Added!")
+
+    
+
+    bot.run(botToken)
 
 
 if __name__ == "__main__":
