5d423482830356a769002cf9e1f2563e01f8e69e
jackablank <jackablank@gmail.com>
2025-01-03 01:10:50 -0800

    returns the author and time of an @everyone jerking off message

--- MeatBeatBot.py
+++ MeatBeatBot.py
@@ -2,6 +2,8 @@ from discord.ext import commands
 import discord
 from dotenv import load_dotenv
 import os
+from datetime import datetime
+import pytz
 
 def main():
     load_dotenv(dotenv_path='C:/MeatBeatBot/botkeys.env')
@@ -11,7 +13,8 @@ def main():
     
 
     bot = commands.Bot(command_prefix='//', intents=discord.Intents.all())
-    
+    timezone = pytz.timezone("US/Pacific")
+
     @bot.event
     async def on_ready():
         print(f'{bot.user} has connected to Discord!')
@@ -21,6 +24,19 @@ def main():
     @bot.command()
     async def add_phrase(ctx):
         await ctx.send("Phrase Added!")
+
+    @bot.listen()
+    async def on_message(message):
+        if message.author == bot.user:
+            return
+        if message.content == '@everyone jerking off':
+            author = message.author
+            timestamp = message.created_at
+
+            localTime = timestamp.astimezone(timezone)
+            formattedTime = localTime.strftime("%m/%d/%Y, %I:%M:%S %p %Z")
+
+            await message.channel.send(f'🍆 User Beat Record Updated:\nUser Jerking It: {author}\nTimestamp: {formattedTime}')
 
     
 
